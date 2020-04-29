e083afeb7c3cecf5a8d1c0187c1b595cdb5a158f
odify <odify495@gmail.com>
2020-04-29 18:39:16 +0200

    buffer-8

--- main.js
+++ main.js
@@ -64,29 +64,54 @@
 // console.log("Exitus");
 
 
-//No.7)
+// //No.7)
 
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
+// var events = require('events');
+// var eventEmitter = new events.EventEmitter();
 
-var listner1 = function listner1() {
-   console.log('listner1 is online...');
+// var listner1 = function listner1() {
+//    console.log('listner1 is online...');
+// }
+// var listner2 = function listner2() {
+//   console.log('listner2 is online...');
+// }
+// eventEmitter.addListener('connection', listner1);
+// eventEmitter.on('connection', listner2);
+// var eventListeners = require('events').EventEmitter.listenerCount
+// (eventEmitter,'connection');
+// console.log(eventListeners + " Listner(s) listening to my online connection event");
+
+// eventEmitter.emit('connection');
+// eventEmitter.removeListener('connection', listner1);
+// console.log("Listner1 is offline...");
+// console.log("Listner2 is offline...");
+
+// eventEmitter.emit('connection');
+// eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
+// console.log(eventListeners + " Listner(s) listening to connection event");
+
+//No.8)
+
+buf = new Buffer(26);
+let x =0;
+for (var x = 0 ; x < 26 ; x++) {
+  buf[x] = x + 97;
 }
-var listner2 = function listner2() {
-  console.log('listner2 is online...');
+console.log( buf.toString('ascii'));       // abcdefghijklmnopqrstuvwxyz
+console.log( buf.toString('ascii',0,5));   // abcde
+console.log( buf.toString('utf8',0,5));    // abcde
+console.log( buf.toString(undefined,0,5));
+
+//No.8-b)
+
+var buffer1 = new Buffer('ABC');
+var buffer2 = new Buffer('ABCD');
+var result = buffer1.compare(buffer2);
+
+if(result < 0) {
+   console.log(buffer1 +" comes before " + buffer2);
+} else if(result === 0) {
+   console.log(buffer1 +" is same as " + buffer2);
+} else {
+   console.log(buffer1 +" comes after " + buffer2);
 }
-eventEmitter.addListener('connection', listner1);
-eventEmitter.on('connection', listner2);
-var eventListeners = require('events').EventEmitter.listenerCount
-(eventEmitter,'connection');
-console.log(eventListeners + " Listner(s) listening to my online connection event");
-
-eventEmitter.emit('connection');
-eventEmitter.removeListener('connection', listner1);
-console.log("Listner1 is offline...");
-console.log("Listner2 is offline...");
-
-eventEmitter.emit('connection');
-eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
-console.log(eventListeners + " Listner(s) listening to connection event");
-
